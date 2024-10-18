06de0dde15a88bd0965ce340980ec3c1826e8ef6
nicolas <158532428+Nicolas2-dev@users.noreply.github.com>
2024-10-18 19:45:48 +0200

    Update Manager.php

--- src/Modules/Manager.php
+++ src/Modules/Manager.php
@@ -2,6 +2,7 @@
 
 namespace Npds\Modules;
 
+use Npds\Http\Request;
 use Npds\Config\Config;
 use Npds\Foundation\AliasLoader;
 
@@ -44,15 +45,18 @@ class Manager
      */
     protected static $bootstrappers_module = [
         'load_config',
-        'load_aliases',         
+        'load_aliases',        
         'load_bootstrap',
         'load_constant',
+        'load_event',
         'load_helper',
         'load_boxe',
-        'load_events',
         'load_route_web',
+        'load_route_web_filter',        
         'load_route_admin',
+        'load_route_admin_filter',        
         'load_route_api',
+        'load_route_api_filter',        
     ];
 
     /**
@@ -61,6 +65,18 @@ class Manager
      * @var boolean
      */
     protected static $has_module_been_bootstrapped = [];
+
+    /**
+     * Undocumented variable
+     *
+     * @var array
+     */
+    protected static $bootedCallbacks = array();
+
+    /**
+     * 
+     */
+    protected static Request $request;
 
 
     /**
@@ -71,6 +87,8 @@ class Manager
         static::$module_path = $directory;
 
         static::$modules = Config::get('modules');
+
+        static::$request = new Request();
     }
 
     /**
@@ -99,23 +117,62 @@ class Manager
         }
 
         foreach (static::$modules as $module) {
-            
-            $classKernel = '\\Modules\\'.ucfirst($module).'\\Bootstrap\\'.ucfirst($module).'Kernel';
+            //
+            static::load_module_bootstrap($module);
+        }
+    }
+
+    /**
+     * Undocumented function
+     *
+     * @param [type] $module
+     * @return object
+     */
+    public static function kernel($module)
+    {
+        return static::$Kernel[ucfirst($module)];
+    }
+
+    /**
+     * Undocumented function
+     *
+     * @param [type] $module
+     * @return string
+     */
+    private static function kernel_modul_class($module)
+    {
+        return 'Modules\\'.ucfirst($module).'\Bootstrap\\'.ucfirst($module).'Kernel';
+    }
+
+    /**
+     * Undocumented function
+     *
+     * @param [type] $module
+     * @return void
+     */
+    private static function load_module_bootstrap($module)
+    {
+        $classKernel = static::kernel_modul_class($module);
               
-            if(class_exists($classKernel) && method_exists($classKernel, 'getInstance')) {
+        if(class_exists($classKernel) && method_exists($classKernel, 'getInstance')) {
 
-                static::$Kernel = [ucfirst($module) => with($classKernel::getInstance(static::$module_path .ucfirst($module). DS))];
+            static::$Kernel = [$module => with($classKernel::getInstance(static::$module_path .$module. DS))];
 
-                static::$has_module_been_bootstrapped = [
-                    ucfirst($module) => false
-                ];
+            foreach (static::$bootstrappers_module as $bootstrapper) {
 
-                static::bootstrap_module(ucfirst($module));
+               static::$bootedCallbacks[$module]['booststrap'][$bootstrapper] = false;
+
+                if (!static::$bootedCallbacks[$module]['booststrap'][$bootstrapper]) {
+
+                    call_user_func_array([static::class, $bootstrapper], [$module, static::$request]);
+
+                    static::$bootedCallbacks[$module]['booststrap'][$bootstrapper] = true;
+                }
+             
             }
 
-            if (isset(static::$Kernel[$module]::$boot_method))  {
-                static::register_boot_method($module);
-            }
+            // 
+            static::load_module_boot_method($module);
         }
     }
 
@@ -125,62 +182,21 @@ class Manager
      * @param [type] $module
      * @return void
      */
-    private static function register_boot_method($module)
+    private static function load_module_boot_method($module)
     {
-        foreach (static::$Kernel[$module]::$boot_method as $module_boot_method) {
-            call_user_func_array([static::$Kernel[$module], 'register_'.$module_boot_method], []);
+        if (isset(static::$Kernel[$module]::$boot_method)){
+            foreach(static::$Kernel[$module]::$boot_method as $bootstrapper) {
+
+                static::$bootedCallbacks[$module]['functions']['register_'.$bootstrapper] = false;
+
+                if (!static::$bootedCallbacks[$module]['functions']['register_'.$bootstrapper]) {
+
+                    call_user_func_array([static::$Kernel[$module], 'register_'.$bootstrapper], [static::$request, static::$Kernel[$module]]);
+
+                    static::$bootedCallbacks[$module]['functions']['register_'.$bootstrapper] = true;
+                }
+            }
         }
-    }
-
-
-    /**
-     * Bootstrap the application for HTTP requests.
-     *
-     * @return void
-     */
-    public static function bootstrap_module($module)
-    {
-        if (! static::has_module_been_bootstrapped($module)) {
-            static::bootstrap_module_with(static::bootstrappers_module(), $module);
-        }
-    }
-
-    /**
-     * Undocumented function
-     *
-     * @param array $bootstrappers
-     * @param [type] $module
-     * @return void
-     */
-    public static function bootstrap_module_with(array $bootstrappers, $module)
-    {
-        foreach ($bootstrappers as $bootstrapper) {
-            static::$bootstrapper($module);
-        }
-
-        static::$has_module_been_bootstrapped = [
-            $module => true
-        ];
-    }
-
-    /**
-     * Undocumented function
-     *
-     * @return boolean
-     */
-    public static function has_module_been_bootstrapped($module)
-    {
-        return static::$has_module_been_bootstrapped[$module];
-    }
-
-    /**
-     * Undocumented function
-     *
-     * @return array
-     */
-    protected static function bootstrappers_module()
-    {
-        return static::$bootstrappers_module;
     }
 
     /**
@@ -201,7 +217,7 @@ class Manager
      *
      * @return  [type]           [return description]
      */
-    public static function load_config($module)
+    private static function load_config($module)
     {
         $filePath = str_replace('/', DS, static::$module_path . $module.'/Config');
         
@@ -218,7 +234,7 @@ class Manager
      *
      * @return  [type]  [return description]
      */
-    public static function load_bootstrap($module)
+    private static function load_bootstrap($module)
     {
         static::load_file('bootstrap.php', $module);
     }
@@ -232,6 +248,17 @@ class Manager
     private static function load_constant($module)
     {
         static::load_file('constants.php', $module);
+    }
+
+    /**
+     * Undocumented function
+     *
+     * @param [type] $module
+     * @return void
+     */
+    private static function load_event($module)
+    {
+        static::load_file('Events/events.php', $module);
     }
 
     /**
@@ -262,20 +289,9 @@ class Manager
      * @param [type] $module
      * @return void
      */
-    private static function load_events($module)
-    {
-        static::load_file('Events/events.php', $module);
-    }
-
-    /**
-     * Undocumented function
-     *
-     * @param [type] $module
-     * @return void
-     */
     private static function load_route_web($module)
     {
-        static::load_file('Routes/web/routes.php', $module);
+        static::load_file('routes/web/routes.php', $module);
     }
 
     /**
@@ -299,6 +315,40 @@ class Manager
     {
         static::load_file('Routes/api/routes.php', $module);
     }
+
+    /**
+     * Undocumented function
+     *
+     * @param [type] $module
+     * @return void
+     */
+    private static function load_route_web_filter($module)
+    {
+        static::load_file('Routes/web/filters.php', $module);
+    }
+
+    /**
+     * Undocumented function
+     *
+     * @param [type] $module
+     * @return void
+     */
+    private static function load_route_admin_filter($module)
+    {
+        static::load_file('Routes/admin/filters.php', $module);
+    }
+
+    /**
+     * Undocumented function
+     *
+     * @param [type] $module
+     * @return void
+     */
+    private static function load_route_api_filter($module)
+    {
+        static::load_file('Routes/api/filters.php', $module);
+    }
+
 
     /**
      * Undocumented function
